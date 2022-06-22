dcdabeb3d6119c7f921517caff680eefe6af3395
차지원 <chajiwon100785@gmail.com>
2022-06-23 01:46:37 +0900

    Refactor: refactor missile make method [#27]

--- model/missile.py
+++ model/missile.py
@@ -1,26 +1,26 @@
 from util.factory import Factory
 
-IMG_PATH = 'img/'
 
 class Missile:
 
-    def __init__(self, missile):
-        self.recMissile = Factory().create_rect_object(missile)
+    def __init__(self):
+        self.missile = Factory().create_object_from_image('missile')
+        self.recMissile = Factory().create_rect_object(self.missile)
 
-    def make_missile(self, recPlayer, isGameOver):
-        if isGameOver:
-            return
-        for i in range(len(self.recMissile)):
-            if self.recMissile[i].y == -1:
-                self.recMissile[i].x = recPlayer.x
-                self.recMissile[i].y = recPlayer.y
+    def make_missile(self, recPlayer):
+        for missile in self.recMissile:
+            if missile.y == -1:
+                missile.x, missile.y = recPlayer.x, recPlayer.y
                 break
 
-    def move_missile(self, isGameOver):
-        for i in range(len(self.recMissile)):
-            if self.recMissile[i].y == -1:
-                continue
-            if not isGameOver:
-                self.recMissile[i].y -= 1
-            if self.recMissile[i].y < 0:
-                self.recMissile[i].y = -1
+    def __get_missile_y(self, y):
+        if y == -1:
+            return y
+        elif y >= 0:
+            return y - 1
+        else:
+            return -1
+
+    def move_missile(self):
+        for missile in self.recMissile:
+            missile.y = self.__get_missile_y(missile.y)
