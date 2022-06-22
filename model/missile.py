6253c7c9001b53b1e3c5eea8c9c1c11c39cd2e04
차지원 <chajiwon100785@gmail.com>
2022-06-23 01:40:53 +0900

    Feat: add factory and edit file structure[#27]

--- model/missile.py
+++ model/missile.py
@@ -1,8 +1,26 @@
+from util.factory import Factory
+
 IMG_PATH = 'img/'
 
 class Missile:
-    def createMissile(self, pygame):
-        missile = [pygame.image.load(IMG_PATH + 'missile.png') for i in range(40)]
-        for i in range(len(missile)):
-            missile[i] = pygame.transform.scale(missile[i], (20, 20))
-        return missile
+
+    def __init__(self, missile):
+        self.recMissile = Factory().create_rect_object(missile)
+
+    def make_missile(self, recPlayer, isGameOver):
+        if isGameOver:
+            return
+        for i in range(len(self.recMissile)):
+            if self.recMissile[i].y == -1:
+                self.recMissile[i].x = recPlayer.x
+                self.recMissile[i].y = recPlayer.y
+                break
+
+    def move_missile(self, isGameOver):
+        for i in range(len(self.recMissile)):
+            if self.recMissile[i].y == -1:
+                continue
+            if not isGameOver:
+                self.recMissile[i].y -= 1
+            if self.recMissile[i].y < 0:
+                self.recMissile[i].y = -1
--- model/star.py
+++ model/star.py
@@ -1,8 +1,43 @@
+import random
+
+from main import SCREEN_WIDTH, SCREEN_HEIGHT
+from util.factory import Factory
+
 IMG_PATH = "img/"
 
+
 class Star:
-    def createStar(self, pygame):
-        star = [pygame.image.load(IMG_PATH + 'star.png') for i in range(40)]
-        for i in range(len(star)):
-            star[i] = pygame.transform.scale(star[i], (20, 20))
-        return star
+
+    def __init__(self, star):
+        self.recStar = Factory().create_rect_object(star)
+        self.delay = 0
+
+    def __delay_star(self):
+        if self.delay > 5:
+            self.delay = 0
+            return True
+
+        else:
+            self.delay += 1
+            return False
+
+    def make_star(self, isGameOver):
+        if isGameOver:
+            return
+        if self.__delay_star():
+            i = random.randint(0, len(self.recStar) - 1)
+            if self.recStar[i].y == -1:
+                self.recStar[i].x = random.randint(0, SCREEN_WIDTH)
+                self.recStar[i].y = 0
+
+    def move_star(self, isGameOver):
+        self.make_star(isGameOver)
+        for i in range(len(self.recStar)):
+            if self.recStar[i].y == -1:
+                continue
+
+            if not isGameOver:
+                self.recStar[i].y += 1
+
+            if self.recStar[i].y > SCREEN_HEIGHT:
+                self.recStar[i].y = -1
