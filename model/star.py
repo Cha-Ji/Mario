5d7a9b22dac8d39cfde5d42e66e544cafaaa9efe
차지원 <chajiwon100785@gmail.com>
2022-06-23 01:47:46 +0900

    Refactor: refactor star make method [#27]

--- model/star.py
+++ model/star.py
@@ -3,13 +3,12 @@ import random
 from main import SCREEN_WIDTH, SCREEN_HEIGHT
 from util.factory import Factory
 
-IMG_PATH = "img/"
-
 
 class Star:
 
-    def __init__(self, star):
-        self.recStar = Factory().create_rect_object(star)
+    def __init__(self):
+        self.star = Factory().create_object_from_image('star')
+        self.recStar = Factory().create_rect_object(self.star)
         self.delay = 0
 
     def __delay_star(self):
@@ -21,23 +20,22 @@ class Star:
             self.delay += 1
             return False
 
-    def make_star(self, isGameOver):
-        if isGameOver:
-            return
+    def make_star(self):
         if self.__delay_star():
             i = random.randint(0, len(self.recStar) - 1)
             if self.recStar[i].y == -1:
                 self.recStar[i].x = random.randint(0, SCREEN_WIDTH)
                 self.recStar[i].y = 0
 
-    def move_star(self, isGameOver):
-        self.make_star(isGameOver)
-        for i in range(len(self.recStar)):
-            if self.recStar[i].y == -1:
-                continue
+    def __get_star_y(self, y):
+        if y == -1:
+            return y
+        elif y >= SCREEN_HEIGHT:
+            return -1
+        else:
+            return y + 1
 
-            if not isGameOver:
-                self.recStar[i].y += 1
-
-            if self.recStar[i].y > SCREEN_HEIGHT:
-                self.recStar[i].y = -1
+    def move_star(self):
+        self.make_star()
+        for star in self.recStar:
+            star.y = self.__get_star_y(star.y)
